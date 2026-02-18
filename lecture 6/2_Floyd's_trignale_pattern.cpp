407a6afeb85668b71257fa67cdb1c979a525904f
Ashu <ashutosh.mishra.royal.max@gmail.com>
2026-02-18 13:09:37 +0530

    Floyd's triangle pattern

---
+++ lecture 6/2_Floyd's_trignale_pattern.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int count = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i; j++)
+        {
+            cout << "   ";
+        }
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            if (count < 10)
+            {
+                cout << count << "  ";
+            }
+            else
+            {
+                cout << count << " ";
+            }
+            count++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
